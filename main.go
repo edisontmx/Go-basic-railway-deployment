dd49f3ae25d6b57d70c89692863465e52bbded95
edisontmx <edison.mc@gmail.com>
2023-06-04 17:40:09 -0500

    agrego archivo .env y variable del puerto

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "hello world: %s\n", r.URL.Path)
 	})
-	http.ListenAndServe(":8080", nil)
+	puerto := os.Getenv("PORT")
+	if puerto == "" {
+		puerto = "8080"
+	}
+	http.ListenAndServe(":"+puerto, nil)
+
 }
